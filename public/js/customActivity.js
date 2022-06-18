9e3ea520fd370da16cdbca95d43bf930c483c35b
viet <nguyenhoangviet171@gmail.com>
2022-06-18 10:25:25 +0700

    First commit

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -118,7 +118,7 @@ define([
         console.log(foundSignupDate);
 
         payload['arguments'].execute.inArguments = [{
-            "caseID": "{{Interaction.REST-1.caseID}}" ,
+            "caseID": "{{Interaction.REST-2.caseID}}" ,
             "message": message ,
             "url": url ,
             "title": title ,
@@ -129,7 +129,7 @@ define([
         }];
 
         payload['arguments'].execute.outArguments = [{
-            "caseID": "{{Interaction.REST-1.caseID}}"
+            "caseID": "{{Interaction.REST-2.caseID}}"
         }];
         
         payload['metaData'].isConfigured = true;
