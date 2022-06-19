84e2bf336e70ae2bc17ca66a91d9d6bd9684ab8d
viet <nguyenhoangviet171@gmail.com>
2022-06-19 07:50:54 +0700

    First commit

--- routes/activity.js
+++ routes/activity.js
@@ -101,7 +101,7 @@ exports.execute = function (req, res) {
             var url = decoded.inArguments[0].url;
             var urlImage = decoded.inArguments[0].urlimg;
             var zaloId = decoded.inArguments[0].ContactID;
-            var caseID = decoded.inArguments[0].caseID;
+            var caseid = decoded.inArguments[0].caseid;
             // var title = decoded.inArguments[0].title.replace('%name%', name);
             // var title = decoded.inArguments[0].title.replace('%name%', name);
 
@@ -142,7 +142,7 @@ exports.execute = function (req, res) {
 
             // res.status(200).send("caseid",message);
             // res.status(200).send('Execute',message);
-             res.status(200).send(body,'Execute');
+             res.status(200).send(caseid,'Execute');
         } else {
             console.error('inArguments invalid.');
             return res.status(400).end();
