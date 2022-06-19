8b8b0d0699a2387c1288ba2951cf0d245319f200
viet <nguyenhoangviet171@gmail.com>
2022-06-19 20:54:24 +0700

    First commit

--- routes/activity.js
+++ routes/activity.js
@@ -142,7 +142,7 @@ exports.execute = function (req, res) {
 
             // res.status(200).send("caseid",message);
             // res.status(200).send('Execute',message);
-             res.status(200).send('Execute' ,{'caseID': message});
+             res.status(200).send(message);
         } else {
             console.error('inArguments invalid.');
             return res.status(400).end();
